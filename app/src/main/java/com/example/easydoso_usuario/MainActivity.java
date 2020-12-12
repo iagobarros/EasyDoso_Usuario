fa2e4231dbf148d7fe010327d6affe2e67d75a0f
iago luis barros <iagobarrosgit@gmail.com>
2020-12-12 19:16:48 -0300

    Primeira Tela

--- app/src/main/java/com/example/easydoso_usuario/MainActivity.java
+++ app/src/main/java/com/example/easydoso_usuario/MainActivity.java
@@ -9,15 +9,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
-    String mTitle[] = {"DIARISTA", "TAXI", "MECÂNICO", "DIARISTA1", "DIARISTA2"};
-
+    String mTitle[] = {"DIARISTA", "TAXI", "MECÂNICO", "DIARISTA1", "DIARISTA2", "DIARISTA3", "DIARISTA4", "DIARISTA5", "DIARISTA6"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,25 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         MyAdapter adapter = new MyAdapter(this, mTitle);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
     }
 
     class MyAdapter extends ArrayAdapter<String> {
@@ -33,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
         Context context;
         String rTitle[];
         MyAdapter(Context c, String title[]) {
-            super(c, R.layout.row, R.id.materialButton, title);
+            super(c, R.layout.row, R.id.textView, title);
             this.context = c;
             this.rTitle = title;
         }
@@ -43,7 +65,9 @@ public class MainActivity extends AppCompatActivity {
         public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
             LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View row = layoutInflater.inflate(R.layout.row, parent, false);
-            TextView myTitle = row.findViewById(R.id.materialButton);
+            //MaterialButton button = (MaterialButton) convertView.findViewById(R.id.materialButton);
+            //button.setText(rTitle[position]);
+            TextView myTitle = row.findViewById(R.id.textView);
             myTitle.setText(rTitle[position]);
             return row;
         }
